d69810eb12fb90f965b1dc85303906ac449e3938
Bahman Movaqar <bahman@bahmanm.com>
2025-03-21 10:28:54 -0700

    Write up a suite of tests for deserialisation

--- lib/src/test/groovy/com/bahmanm/pgn/DeserialiserSpec.groovy
+++ lib/src/test/groovy/com/bahmanm/pgn/DeserialiserSpec.groovy
@@ -1,54 +1,475 @@
 package com.bahmanm.pgn
 
+import groovy.util.logging.Slf4j
 import spock.lang.Specification
 
 import com.bahmanm.pgn.models.*
 
+@Slf4j
 class DeserialiserSpec extends Specification {
 
-  def 'should return null when passed an empty text'() {
-    setup:
-    def deserialiser = new Deserialiser()
+    def "test empty input"() {
+        setup:
+        Deserialiser deserialiser = new Deserialiser()
 
-    when:
-    def result = deserialiser.apply('')
+        when:
+        deserialiser.deserialise(null)
 
-    then:
-    result == null
-  }
+        then:
+        thrown(IllegalArgumentException)
 
-  def 'should be able to deserialise a perfectly formed PGN file'() {
-    setup:
-    def deserialiser = new Deserialiser()
-    def text = deserialiser.class.getResource('/perfectly-formed.pgn')
+        when:
+        deserialiser.deserialise("")
 
-    when:
-    def result = deserialiser.apply(text)
+        then:
+        thrown(IllegalArgumentException)
+    }
 
-    then:
-    result
-  }
+    def "test minimal game"() {
+        setup:
+        String pgn = """
+            [Event "Test"]
+            [Site "Home"]
+            [Date "2024.01.26"]
+            [Round "?"]
+            [White "Me"]
+            [Black "You"]
+            [Result "*"]
 
-  def 'should be able to deserialise a minimal PGN'() {
-    setup:
-    def deserialiser = new Deserialiser()
-    def text = '''
-[White "A"]
-[Black "B"]
-[Result "1/2-1/2"]
+            *
+            """
+        Deserialiser deserialiser = new Deserialiser()
 
-1. d4 d5 2. c4 e6 3. Nc3 Nf6 4. Bg5 Be7 5. Nf3 1/2-1/2
-'''
-    when:
-    def result = deserialiser.apply(text)
+        when:
+        Game game = deserialiser.deserialise(pgn)
 
-    then:
-    result.tags.size() == 3
-    new Tag('White', '"A"') in result.tags
-    new Tag('Black', '"B"') in result.tags
-    new Tag('Result', '"1/2-1/2"') in result.tags
-    result.result == "1/2-1/2"
-    result.startingMoveNumber == 1
-    result.firstPly
-  }
+        then:
+        game != null
+        game.tags.size() == 7
+        
+        game.tags.find { it.key == "Event" }.value == "Test"
+        game.result == "*"
+        game.firstPly == null
+        game.startingMoveNumber == 1
+    }
+
+    def "test single move game"() {
+        setup:
+        String pgn = """
+            [Event "Test"]
+            [Site "Home"]
+            [Date "2024.01.26"]
+            [Round "?"]
+            [White "Me"]
+            [Black "You"]
+            [Result "*"]
+
+            1. e4 *
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        game.tags.size() == 7
+        game.tags.find { it.key == "Event" }.value == "Test"
+        game.result == "*"
+        game.firstPly != null
+        game.firstPly.san == "e4"
+        game.firstPly.next == null
+        game.startingMoveNumber == 1
+    }
+
+    def "test two move game"() {
+        setup:
+        String pgn = """
+            [Event "Test"]
+            [Site "Home"]
+            [Date "2024.01.26"]
+            [Round "?"]
+            [White "Me"]
+            [Black "You"]
+            [Result "*"]
+
+            1. e4 c5 *
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        game.tags.size() == 7
+        game.tags.find { it.key == "Event" }.value == "Test"
+        game.result == "*"
+        game.firstPly != null
+        game.firstPly.san == "e4"
+        game.firstPly.next != null
+        game.firstPly.next.san == "c5"
+        game.firstPly.next.next == null
+        game.startingMoveNumber == 1
+    }
+
+    def "test game with result"() {
+        setup:
+        String pgn = """
+            [Event "Test"]
+            [Site "Home"]
+            [Date "2024.01.26"]
+            [Round "?"]
+            [White "Me"]
+            [Black "You"]
+            [Result "1-0"]
+
+            1. e4 c5 2. Nf3 d6 1-0
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        game.result == "1-0"
+        game.startingMoveNumber == 1
+    }
+
+    def "test game with comments"() {
+        setup:
+        String pgn = """
+            [Event "Test"]
+            [Site "Home"]
+            [Date "2024.01.26"]
+            [Round "?"]
+            [White "Me"]
+            [Black "You"]
+            [Result "*"]
+
+            1. e4 {This is a comment} c5 {Another comment} 2. Nf3 d6 *
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        Ply firstPly = game.firstPly
+        firstPly.commentBefore == "This is a comment"
+        firstPly.next.commentBefore == "Another comment"
+        game.startingMoveNumber == 1
+    }
+
+    def "test game with variations"() {
+        setup:
+        String pgn = """
+            [Event "Test"]
+            [Site "Home"]
+            [Date "2024.01.26"]
+            [Round "?"]
+            [White "Me"]
+            [Black "You"]
+            [Result "*"]
+
+            1. e4 (1. d4) c5 2. Nf3 d6 *
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        Ply firstPly = game.firstPly
+        firstPly.san == "e4"
+        firstPly.variations.size() == 1
+        firstPly.variations[0].san == "d4"
+        game.firstPly.next.san == "c5"
+        game.startingMoveNumber == 1
+    }
+
+    def "test game with nested variations"() {
+        setup:
+        String pgn = """
+            [Event "Test"]
+            [Site "Home"]
+            [Date "2024.01.26"]
+            [Round "?"]
+            [White "Me"]
+            [Black "You"]
+            [Result "*"]
+
+            1. e4 (1. d4 (1. c4)) c5 *
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        Ply firstPly = game.firstPly;
+        firstPly.san == "e4";
+        firstPly.variations.size() == 1;
+        firstPly.variations[0].san == "d4";
+        firstPly.variations[0].variations.size() == 1;
+        firstPly.variations[0].variations[0].san == "c4";
+        game.startingMoveNumber == 1
+    }
+
+    def "test game with nag"() {
+        setup:
+        String pgn = """
+            [Event "Test"]
+            [Site "Home"]
+            [Date "2024.01.26"]
+            [Round "?"]
+            [White "Me"]
+            [Black "You"]
+            [Result "*"]
+
+            1. e4 \$1 c5 \$2 2. Nf3 d6 *
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        Ply firstPly = game.firstPly
+        firstPly.san == "e4"
+        firstPly.nag == "\$1"
+        game.firstPly.next.san == "c5"
+        game.firstPly.next.nag == "\$2"
+        game.startingMoveNumber == 1
+    }
+
+    def "test real game"() {
+        setup:
+        String pgn = """
+            [Event "FIDE World Championship Match 2023"]
+            [Site "Astana KAZ"]
+            [Date "2023.04.09"]
+            [Round "1"]
+            [White "Ding Liren"]
+            [Black "Nepomniachtchi, Ian"]
+            [Result "1/2-1/2"]
+            [ECO "D37"]
+            [WhiteElo "2788"]
+            [BlackElo "2795"]
+            [PlyCount "90"]
+
+            1. d4 Nf6 2. c4 e6 3. Nf3 d5 4. Nc3 Be7 5. Bf4 O-O 6. e3 c5 7. dxc5 Bxc5 8. a3 Nc6 9. Qc2 Re8 10. Be2 h6 11. O-O a6 12. h3 Ba7 13. Rfd1 Qe7 14. cxd5 exd5 15. b4 Be6 16. Rac1 Rac8 17. Qb1 Bb8 18. Bxb8 Rxb8 19. b5 axb5 20. Nxb5 Ne4 21. Qb2 Rec8 22. Nfd4 Qf6 23. Bf3 Ne5 24. Bxe4 dxe4 25. Nxe6 fxe6 26. Rxc8+ Rxc8 27. Nd4 Nd3 28. Qa2 Re8 29. a4 Kh8 30. a5 Rf8 31. Rf1 Re8 32. Qc4 Qg6 33. Qc7 e5 34. Nb5 Qe6 35. Nd6 Rf8 36. Nxe4 Qd5 37. Ng3 b5 38. axb6 Qb3 39. Ne4 Qd5 40. Ng3 Qb3 41. Ne4 Qd5 42. Qc2 Nb4 43. Qb1 Nc6 44. Rd1 Qc4 45. Qd3 Qa4 1/2-1/2
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        game.tags.size() == 13
+        game.result == "1/2-1/2"
+        game.firstPly.san == "d4"
+        game.firstPly.next.san == "Nf6"
+        game.startingMoveNumber == 1
+    }
+
+    def "test game with missing tags"() {
+        setup:
+        String pgn = "1. e4 c5 *"
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        game.tags.size() == 0
+        game.firstPly.san == "e4"
+        game.startingMoveNumber == 1
+    }
+
+    def "test game with extra whitespace"() {
+        setup:
+        String pgn = """
+            [Event  "Test"  ]
+            [Site  "Home"  ]
+
+            1.  e4    c5  *
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        game.tags.size() == 2
+        game.firstPly.san == "e4"
+        game.startingMoveNumber == 1
+    }
+
+    def "test game with only result"() {
+        setup:
+        String pgn = "1-0"
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        game.result == "1-0"
+        game.firstPly == null
+        game.startingMoveNumber == 1
+    }
+
+    def "test game with multiple variations"() {
+        setup:
+        String pgn = """
+            1. e4 (1. d4) (1. c4) c5 *
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        Ply firstPly = game.firstPly
+        firstPly.san == "e4"
+        firstPly.variations.size() == 2
+        firstPly.variations[0].san == "d4"
+        firstPly.variations[1].san == "c4"
+        game.startingMoveNumber == 1
+    }
+
+    def "test game with variations and comments"() {
+        setup:
+        String pgn = """
+            1. e4 (1. d4 {D Queen's Pawn}) c5 {Sicilian} *
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        Ply firstPly = game.firstPly
+        firstPly.san == "e4"
+        firstPly.variations.size() == 1
+        firstPly.variations[0].san == "d4"
+        firstPly.variations[0].commentBefore == "D Queen's Pawn"
+        game.firstPly.next.san == "c5"
+        game.firstPly.next.commentBefore == "Sicilian"
+        game.startingMoveNumber == 1
+    }
+
+    def "test game with long NAG"() {
+        setup:
+        String pgn = """
+            [Event "Test"]
+            [Site "Test"]
+            [Date "2024.02.09"]
+            [Round "1"]
+            [White "Player1"]
+            [Black "Player2"]
+            [Result "*"]
+            
+            1. e4 \$1234 c5 *
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        game.firstPly.nag == "\$1234"
+        game.firstPly.san == "e4"
+        game.startingMoveNumber == 1
+    }
+
+    def "test game with move number with three dots"() {
+        setup:
+        String pgn = """
+            [Event "Test"]
+            [Site "Test"]
+            [Date "2024.02.09"]
+            [Round "1"]
+            [White "Player1"]
+            [Black "Player2"]
+            [Result "*"]
+            
+            1... c5 2. Nf3 d6 *
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        game.firstPly.san == "c5"
+        game.firstPly.next.san == "Nf3"
+        game.startingMoveNumber == 1
+    }
+
+    def "test game starting from ply 2, white moves first"() {
+        setup:
+        String pgn = """
+            [Event "Test"]
+            [Site "Test"]
+            [Date "2024.02.10"]
+            [Round "1"]
+            [White "Player1"]
+            [Black "Player2"]
+            [Result "*"]
+            [StartPly "2"]
+            
+            2. e4 c5 *
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        game.firstPly.san == "e4"
+        game.firstPly.next.san == "c5"
+        game.startingMoveNumber == 2
+    }
+
+    def "test game starting from ply 2, black moves first"() {
+        setup:
+        String pgn = """
+            [Event "Test"]
+            [Site "Test"]
+            [Date "2024.02.10"]
+            [Round "1"]
+            [White "Player1"]
+            [Black "Player2"]
+            [Result "*"]
+            [StartPly "2"]
+            
+            2... c5 3. e4 *
+            """
+        Deserialiser deserialiser = new Deserialiser()
+
+        when:
+        Game game = deserialiser.deserialise(pgn)
+
+        then:
+        game != null
+        game.firstPly.san == "c5"
+        game.firstPly.next.san == "e4"
+        game.startingMoveNumber == 2
+    }
 }
